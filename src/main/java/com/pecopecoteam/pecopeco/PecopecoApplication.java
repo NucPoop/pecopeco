5e9e816cad395a47c8da739766c96dfa1acfcc37
NucPoop <kjm5546@gmail.com>
2021-01-24 14:58:06 +0900

    [add] first commit

---
+++ src/main/java/com/pecopecoteam/pecopeco/PecopecoApplication.java
@@ -0,0 +1,13 @@
+package com.pecopecoteam.pecopeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecopecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecopecoApplication.class, args);
+	}
+
+}
